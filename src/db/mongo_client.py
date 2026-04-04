510d9fcb36ab6dde80df942f1743d681dc126157
triet-bit <911huynhminhtriet2019@gmail.com>
2026-04-04 18:37:40 +0700

    triet

--- src/db/mongo_client.py
+++ src/db/mongo_client.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient, errors as mongo_errors
 from src.utils.logger import log
 from dotenv import load_dotenv
 from typing import Optional
-import ssl  
 
 load_dotenv()
 
@@ -21,15 +20,12 @@ def get_db() -> pymongo.database.Database:
         log.info("Reusing existing MongoDB connection")
         return _db
     try:
-        ssl_context = ssl.create_default_context()
-        ssl_context.check_hostname = False
-        ssl_context.verify_mode = ssl.CERT_NONE
 
         _client = MongoClient(
             MONGO_URI,
             tls=True,
             tlsAllowInvalidCertificates=True,
-            ssl_context=ssl_context,
+            tlsInsecure=True,
             serverSelectionTimeoutMS=5000,
         )
         _client.admin.command("ping")
